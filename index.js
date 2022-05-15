5b18998c774760d09c39b8396958d8eb9a6690db
Glitch (obsidian-seed-polka) <none>
2022-05-15 09:02:48 +0000

    🌽🚿 Checkpoint ./database.js:3429589/5821 ./package.json:3429589/51 ./index.js:3429589/336

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Playoff = require('./playoff.js');
+const Db = require('./database.js')
 
 const asyncHandler = require('express-async-handler')
 require("json-circular-stringify");
@@ -25,6 +26,16 @@ app.get('/inscripciones', asyncHandler(async(req, res)=>{
   const dni = req.params.dni;
   const cursos = await Playoff.inscripciones(await getToken());
   res.send(cursos);
+}))
+
+app.get('/asistencia/:prof/:fecha/:alumno', asyncHandler(async(req, res)=>{
+  const prof = req.params.prof;
+  const fecha = req.params.fecha;
+  const alumno = req.params.alumno;
+  
+  const asistencias = await Db.getAsistencia(prof, fecha, alumno);
+  res.send(asistencias)
+  
 }))
 
 
